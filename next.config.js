0f75d5d3e475fb5c2df6b48ac0f27bd67b185de0
Yash Purkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-13 20:35:22 +0530

    Revert "Basic setup"

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-/** @type {import('next').NextConfig} 
- * DB_LOCAL_URI - Default port for mongodb to run locally.
-*/
-const nextConfig = {
-    env: {
-        DB_LOCAL_URI : 'mongodb://127.0.0.1:27017/book-it',
-        DB_URI : ""
-    }
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
 module.exports = nextConfig
