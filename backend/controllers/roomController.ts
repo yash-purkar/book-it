ebbb22700d5f107b8dffbddd579f82f9533e22ac
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-14 12:47:33 +0530

    feat(book-it/backend): Get all rooms

--- backend/controllers/roomController.ts
+++ backend/controllers/roomController.ts
@@ -1,9 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import Room from "@/backend/models/room";
 
+
 export const getAllRooms = async (request: NextRequest) => {
+
+    // For pagination, 8 results per page
+    const resultsPerPage:number = 8;
+
+    const rooms = await Room.find();
+
   return NextResponse.json({
-    data: "Again Working Fine!!",
+    Success:true,
+    resultsPerPage,
+    rooms
   });
 };
 
