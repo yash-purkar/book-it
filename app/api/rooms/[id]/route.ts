eb19a8a85fcb3608e327c0104b8a8a768a691809
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-14 14:59:16 +0530

    feat(book-it/backend): Controller and Route for room details

---
+++ app/api/rooms/[id]/route.ts
@@ -0,0 +1,20 @@
+import connectToDB from '@/backend/config/db.Connect';
+import { getRoomDetails } from '@/backend/controllers/roomController';
+import {createEdgeRouter} from 'next-connect';
+import { NextRequest } from 'next/server';
+
+interface RequestContext {
+    params:{
+        id:string
+    }
+};
+
+const router = createEdgeRouter<NextRequest,RequestContext>();
+
+connectToDB();
+
+router.get(getRoomDetails);
+
+export const GET = async(request:NextRequest,ctx:RequestContext) => {
+    return router.run(request,ctx);
+}
--- backend/controllers/roomController.ts
+++ backend/controllers/roomController.ts
@@ -1,18 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import Room from "@/backend/models/room";
 
-
 export const getAllRooms = async (request: NextRequest) => {
+  // For pagination, 8 results per page
+  const resultsPerPage: number = 8;
 
-    // For pagination, 8 results per page
-    const resultsPerPage:number = 8;
-
-    const rooms = await Room.find();
+  const rooms = await Room.find();
 
   return NextResponse.json({
-    Success:true,
+    Success: true,
     resultsPerPage,
-    rooms
+    rooms,
   });
 };
 
@@ -25,4 +23,21 @@ export const addNewRoom = async (request: NextRequest) => {
     Success: true,
     newRoom,
   });
+};
+
+export const getRoomDetails = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+  if (!room) {
+    return NextResponse.json(
+      {
+        error: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ Success: true, room });
 };
