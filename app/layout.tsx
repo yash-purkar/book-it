1dfbb5a9e44f39eeaa11c8e6f378a2d9fe0e5188
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-17 21:53:35 +0530

    feat(book-it/frontend): Added header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import "./globals.css";
 import { Head } from "./head";
 import { GlobalProvider } from "./GlobalProvider";
 import Script from "next/script";
+import { Header } from '@/components/layout/header/Header';
+import { Footer } from '@/components/layout/footer/Footer';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +25,11 @@ export default function RootLayout({
     <html lang="en">
       <Head />
       <body className={inter.className}>
-        <GlobalProvider>{children}</GlobalProvider>
+        <GlobalProvider>
+          <Header/>
+          {children}
+        <Footer/> 
+          </GlobalProvider>
 
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></Script>
         <Script src="https://kit.fontawesome.com/6dca93c62b.js"></Script>
---
+++ components/layout/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="py-1 pt-5">
+      <p className="text-center mt-1">
+        Book IT - 2019-2021, All Rights Reserved
+      </p>
+    </footer>
+  );
+};
