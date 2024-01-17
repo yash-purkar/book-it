ec2f047552a0af38fc027cce7f0c11742c1f19d0
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-17 20:01:46 +0530

    feat(book-it/backend): Added global provider , head and bootstrap and font awesome

---
+++ app/GlobalProvider.tsx
@@ -0,0 +1,5 @@
+import React, { ReactNode } from "react";
+
+export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+  return <>{children}</>;
+};
---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Head = () => {
+  return (
+    <>
+    <title>Book It</title>
+    </>
+  )
+}
