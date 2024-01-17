4bd6b8b1b4e3308ebcb6e52e600f4c14555176e6
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-17 22:00:51 +0530

    style(book-it/frontend): Update footer styling

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import styles from './footer.module.css'
 export const Footer = () => {
   return (
-    <footer className="py-1 pt-5">
+    <footer className={`${styles.footer} py-1 pt-5`}>
       <p className="text-center mt-1">
         Book IT - 2019-2021, All Rights Reserved
       </p>
