1a2a0337cdbbe45372f2051adbb334c2afa2d871
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-20 14:04:40 +0530

    feat(book-it/frontend): Revalidation and notes for the same

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { Home } from '@/components/layout/home/Home'
 
+// export const revalidate = 10;
+// In this case we don't need to pass it in fetch.
+
 const getRoomsData = async () => {
 try {
-  const response = await fetch('http://localhost:3000/api/rooms',{cache:'no-cache'});
+  const response = await fetch('http://localhost:3000/api/rooms',{next:{
+    revalidate:10
+  }});
   return await response.json();
 } catch (error) {
   console.log(error);
@@ -11,6 +16,7 @@ try {
 
 export default async function HomePage() {
   const rooms = await getRoomsData();
+  console.log("Revalidate => ", rooms.resultsPerPage);
   return (
     <>
     <Home/>
