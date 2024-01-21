60ad9436649295f4440e733149fefd9d056dcd8c
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-21 22:30:18 +0530

    feat(book-it/frontend): Added filter for search by Guest capacity and room type

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -1,25 +1,44 @@
-"use client"
-import React, { useState } from 'react'
-import styles from './search.module.css'
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useState } from "react";
+import styles from "./search.module.css";
+import { useRouter } from "next/navigation";
 
 export const Search = () => {
-    const [location,setLocation] = useState<string>("");
-    const router = useRouter();
+  const [location, setLocation] = useState<string>();
+  const [numOfGuests, setNumOfGuests] = useState<number>();
+  const [roomType, setRoomType] = useState<string>();
+  const router = useRouter();
 
-    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        router.push(`/?location=${location}`)
-    }
+    // It'll filter out the locations which has no value
+    const queryString = [
+      location && `location=${location}`,
+      numOfGuests && `guestCapacity=${numOfGuests}`,
+      roomType && `category=${roomType}`,
+    ]
+      .filter(Boolean)
+      .join("&");
+
+    router.push(`/?${queryString}`);
+  };
 
   return (
-    <div className={`row ${styles['wrapper']} mt-5`}>
+    <div className={`row ${styles["wrapper"]} mt-5`}>
       <div className="col-10 col-lg-5">
-        <form onSubmit={handleSubmit} className="shadow rounded" action="#" method="POST">
+        <form
+          onSubmit={handleSubmit}
+          className="shadow rounded"
+          action="#"
+          method="POST"
+        >
           <h2 className="mb-3">Search Rooms</h2>
           <div className="form-group mt-3">
-            <label htmlFor="location_field" className="mb-1"> Location </label>
+            <label htmlFor="location_field" className="mb-1">
+              {" "}
+              Location{" "}
+            </label>
             <input
               type="text"
               className="form-control"
@@ -31,29 +50,49 @@ export const Search = () => {
           </div>
 
           <div className="form-group mt-3">
-            <label htmlFor="guest_field" className="mb-1"> No. of Guests </label>
-            <select className="form-select" id="guest_field">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
+            <label htmlFor="guest_field" className="mb-1">
+              {" "}
+              No. of Guests{" "}
+            </label>
+            <select
+              className="form-select"
+              id="guest_field"
+              onChange={(e) => setNumOfGuests(Number(e.target.value))}
+              value={numOfGuests}
+            >
+              <option value="">--Guest Capicity--</option>
+              {[1, 2, 3, 4, 5, 6].map((num) => (
+                <option value={num}>{num}</option>
+              ))}
             </select>
           </div>
 
           <div className="form-group mt-3">
-            <label htmlFor="room_type_field" className="mb-1"> Room Type </label>
-            <select className="form-select" id="room_type_field">
-              <option value="King">King</option>
-              <option value="Single">Single</option>
-              <option value="Twins">Twins</option>
+            <label htmlFor="room_type_field" className="mb-1">
+              {" "}
+              Room Type{" "}
+            </label>
+            <select
+              className="form-select"
+              id="room_type_field"
+              onChange={(e) => setRoomType(e.target.value)}
+              value={roomType}
+            >
+              <option value="">--Room Type--</option>
+              {["King", "Single", "Twins"].map((value) => (
+                <option value={value}>{value}</option>
+              ))}
             </select>
           </div>
 
-          <button type="submit" className={`btn ${styles['form-btn']} w-100 py-2`}>Search</button>
+          <button
+            type="submit"
+            className={`btn ${styles["form-btn"]} w-100 py-2`}
+          >
+            Search
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
