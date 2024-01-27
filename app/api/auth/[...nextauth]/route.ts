97b48b9ae2036df2ec4cfd57550b3f9265da83ca
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-01-27 10:03:22 +0530

    feat(book-it/backend): Authentication with next-auth (some issue with this)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,13 @@ import User, { IUser } from "@/backend/models/user";
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-
+import bcrypt from "bcryptjs";
+ 
 interface Creds {
   email: string;
 password: string;
 }
-
+ 
 const auth = async (request: NextApiRequest, response: NextApiResponse) => {
   return await NextAuth({
     session: {
@@ -20,24 +20,24 @@ const auth = async (request: NextApiRequest, response: NextApiResponse) => {
         // @ts-ignore
         async authorize(credentials: Creds) {
           connectToDB();
-
+ 
           const { email, password } = credentials;
-
+ 
           const user = await User.findOne({ email });
-
+ 
           if (!user) {
-            throw new Error("Invalid Email or Password!");
+            return null;
           }
-
+ 
           const isPasswordMatched = await bcrypt.compare(
             password,
             user.password
           );
-
+ 
           if (!isPasswordMatched) {
-            throw new Error("Invalid Email or Password!");
+           return null;
           }
-
+ 
           return user;
         },
       }),
@@ -47,20 +47,24 @@ const auth = async (request: NextApiRequest, response: NextApiResponse) => {
             if(user) {
                 token.user = user;
             }
-
+ 
             // TODO : Update session when user is updated.
             return token;
         },
         session: async({session,token}) => {
             session.user = token.user as IUser;
-
+ 
             return session;
         }
     },
     secret: process.env.NEXTAUTH_SECRET
   });
-
+ 
 };
-
-
+ 
+ 
 export {auth as GET, auth as POST};
+// We need to export this function with HTTP method name.
+// We need to export this function with HTTP method name.
+// So nextjs automatically sends those request. So we don't need to send any request to next auth.
+// When user enters creds and clicks on login, authorize function get called.
