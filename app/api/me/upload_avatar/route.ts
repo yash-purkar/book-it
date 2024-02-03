b6a3ced3a0bbeaac952d763bed5501313f69bc8a
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-03 20:16:46 +0530

    feat(book-it/backend): Created controller and router for user avatar upload

---
+++ app/api/me/upload_avatar/route.ts
@@ -0,0 +1,17 @@
+import connectToDB from "@/backend/config/db.Connect";
+import { uploadAvatar } from "@/backend/controllers/userController";
+import { isAuthenticated } from "@/backend/middlewares/auth";
+import { createEdgeRouter } from "next-connect";
+import { NextRequest } from "next/server";
+
+interface RequestContext {}
+
+const router = createEdgeRouter<NextRequest, RequestContext>();
+
+connectToDB();
+
+router.use(isAuthenticated).put(uploadAvatar);
+
+export const PUT = async (request: NextRequest, ctx: RequestContext) => {
+  return router.run(request, ctx);
+};
