ac239ebb381c95e804d38f6745d8de9d7c1e153e
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-03 13:23:43 +0530

    feat(book-it/cloudinary): Setup cloudinary

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
-/** @type {import('next').NextConfig}
- * DB_LOCAL_URI - Default port for mongodb to run locally.
- */
+require('dotenv').config();
+
 const nextConfig = {
-  env: {
-    API_URL: "http://localhost:3000",
-    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/book-it",
-    DB_URI: " ",
-    NEXTAUTH_URL: "http://localhost:3000",
-    NEXTAUTH_SECRET: "Heyy, I wanna join flipkart without dsa",
-  },
   images: {
-    remotePatterns: [{
-      hostname:"res.cloudinary.com",
-    }],
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 module.exports = nextConfig;
