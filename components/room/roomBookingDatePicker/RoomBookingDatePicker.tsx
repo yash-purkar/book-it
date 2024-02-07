d13f90e9cd885c8349ceb1020721f2e65d09e69f
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-07 19:29:50 +0530

    feat(book-it/backend): Added date picker to select the room booking dates

--- components/room/roomBookingDatePicker/RoomBookingDatePicker.tsx
+++ components/room/roomBookingDatePicker/RoomBookingDatePicker.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
+"use client";
+import { IRoom } from "@/backend/models/room";
+import React, { useState } from "react";
+import styles from "./roomBookingDetails.module.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export const RoomBookingDatePicker = () => {
-  return (
-    <div className="booking-card shadow p-4">
-    <p className="price-per-night">
-      <b>$100</b> / night
-    </p>
-    <hr />
-    <p className="mt-5 mb-3">Pick Check In & Check Out Date</p>
-    {/* <!-- Booking Date Picker goes here --> */}
-    {/* <!-- Booking success/error messages go here --> */}
-  </div>
-  )
+interface RoomBookingDatePickerProps {
+  room: IRoom;
 }
+
+export const RoomBookingDatePicker = ({ room }: RoomBookingDatePickerProps) => {
+  const [checkInDate, setCheckInDate] = useState<Date>(new Date());
+  const [checkoutDate, setCheckoutDate] = useState<Date|null>(null);
+
+  const handleDateChange = (dates: Date[]) => {
+    const [checkIn, checkOut] = dates;
+    console.log({ checkIn, checkOut });
+    if (checkIn && checkOut) {
+      setCheckInDate(checkIn);
+      setCheckoutDate(checkOut);
+    }
+  };
+  return (
+    <div className={`${styles["booking-card"]} shadow p-4`}>
+      <p className={styles["price-per-night"]}>
+        <b>${room.pricePerNight}</b> / night
+      </p>
+      <hr />
+      <p className="mt-5 mb-3">Pick Check In & Check Out Date</p>
+      <DatePicker
+        className="w-100"
+        selected={checkInDate}
+        onChange={handleDateChange}
+        startDate={checkInDate}
+        endDate={checkoutDate}
+        selectsRange
+        inline
+      />
+      {/* <!-- Booking success/error messages go here --> */}
+    </div>
+  );
+};
